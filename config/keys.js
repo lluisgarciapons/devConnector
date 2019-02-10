f6adb652be8b82f686f23848f59dad3d3edfb664
Lluis Garcia <llugarciapons@gmail.com>
2019-02-10 18:27:09 +0100

    user and profile GET and POST done

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://lluis:lluis123456@ds127995.mlab.com:27995/devsocialtutorial"
+    "mongodb://lluis:lluis123456@ds127995.mlab.com:27995/devsocialtutorial",
+  secretOrKey: "SecretForJwt"
 };
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -7,17 +9,24 @@ const posts = require("./routes/api/posts");
 
 const app = express();
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // DB config
 const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB
 mongoose
-  .connect(db)
+  .connect(db, { useNewUrlParser: true })
   .then(() => console.log(`mongoDB Connected`))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("hello"));
+// Passport middleware
+app.use(passport.initialize());
 
+// Passport Config
+require("./config/passport")(passport);
 // Use routes
 app.use("/api/users", users);
 app.use("/api/profile", profile);
